83661ed8c327573720bbed77bd780c831712c00c
justSmK <28645140+justSmK@users.noreply.github.com>
2023-05-21 20:34:30 +0300

    Create project

---
+++ UIKit Marathon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit Marathon
+//
+//  Created by Sergei Semko on 5/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
